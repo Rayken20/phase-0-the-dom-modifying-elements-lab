4ef5174ce7190d9e43da92736f8ccc3db4fe2bfa
Rachael <rachael92njoki@gmail.com>
2024-01-03 14:00:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const testVar = "Hi"
+
+// to remove the node element
+document.querySelector("main#main").remove();
+
+// assign it to newHeader 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Rachael is the champion!";
